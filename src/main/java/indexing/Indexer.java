3cadc1a742eeb82e57d9516f3d27b94b182ad166
Schnick <rsc@sybit.de>
2021-05-25 13:11:08 +0200

    indexer + service

---
+++ src/main/java/indexing/Indexer.java
@@ -0,0 +1,23 @@
+package indexing;
+
+public class Indexer {
+
+    public Indexer() {
+    }
+
+    public void close() {
+
+    }
+/**
+    private Document getDocument() {
+    }
+
+    private String pdfContent() {
+    }
+**/
+    public void indexFile() {
+    }
+
+    public void deleteIndex() {
+    }
+}
---
+++ src/main/java/indexing/Service.java
@@ -0,0 +1,20 @@
+package indexing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Service {
+
+    private Indexer indexer;
+
+    public void createPdfIndex() {
+    }
+
+    public Set<Integer> searchInPdfFiles()  {
+        Set<Integer> set = new HashSet<>();
+        return set;
+    }
+
+    public void deletePdfIndex() {
+    }
+}
